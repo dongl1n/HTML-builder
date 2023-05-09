fe6c5fc755abfe7e7429d3856e0a535e09e451ac
Dongl1n <nastya341151@gmail.com>
2023-05-09 11:28:16 +0300

    fix: fix directory in 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-let stream = fs.createWriteStream('text.txt');
-console.log("Введите текст: ");
+let stream = fs.createWriteStream('./02-write-file/text.txt');
+console.log("Введите текст (для выхода нажмите ctrl+c или напишите exit): ");
 process.stdin.on("data", data => {
     if(data.toString().includes("exit")) {
         process.stdout.write("\nКонец записи");
